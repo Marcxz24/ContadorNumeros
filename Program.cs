e93c13f440e21609dc5474782faa868b35c47829
Marcxz24 <marco666.2412@gmail.com>
2025-10-18 17:13:50 -0300

    Adição dos campos de decrementar

--- Program.cs
+++ Program.cs
@@ -14,6 +14,10 @@ namespace ContadorNumeros
             Console.WriteLine("[2] - Para Contar de Cinco em Cinco");
             Console.WriteLine("[3] - Para Contar de Dez em Dez");
             Console.WriteLine(new string('-', 60));
+            Console.WriteLine("[4] - Para Decrementar Número de Um em Um");
+            Console.WriteLine("[5] - Para Decrementar Número de Cinco em Cinco");
+            Console.WriteLine("[6] - Para Decrementar Número de Dez em Dez");
+            Console.WriteLine(new string('-', 60));
 
             string continuar;
 
@@ -59,6 +63,18 @@ namespace ContadorNumeros
                         ContarDezEmDez();
                         break;
 
+                    case 4:
+                        DecrementarUmEmUm();
+                        break;
+                    
+                    case 5:
+                        DecrementarCincoEmCinco();
+                        break;
+
+                    case 6:
+                        DecrementarDezEmDez();
+                        break;
+                    
                     default:
                         Console.WriteLine("Escolha uma das Opções Destacadas...");
                         Console.WriteLine(new string('-', 60));
@@ -85,7 +101,7 @@ namespace ContadorNumeros
             Console.ReadKey();
         }
 
-        static int PedirNumero()
+        static int PedirNumeroContar()
         {
             int numeroContar = 0;
 
@@ -110,9 +126,35 @@ namespace ContadorNumeros
             return numeroContar;
         }
 
+        static int PedirNumeroDecrementar()
+        {
+            int numeroDecrementar = 0;
+
+            bool validarNumeroDecrementar = false;
+
+            while (!validarNumeroDecrementar)
+            {
+                Console.Write("Digite Um Número Para Decrementar: ");
+                string numeroDigitadoInicial = Console.ReadLine();
+                Console.WriteLine(new string('-', 60));
+
+                if (int.TryParse(numeroDigitadoInicial, out numeroDecrementar))
+                {
+                    validarNumeroDecrementar = true;
+                }
+                else
+                {
+                    Console.WriteLine("Valor Inválido, Digite Apenas Números!");
+                    Console.WriteLine(new string('-', 60));
+                }
+            }
+
+            return numeroDecrementar;
+        }
+
         static void ContarUmEmUm()
         {
-            int numeroFinal = PedirNumero();
+            int numeroFinal = PedirNumeroContar();
 
             for (int i = 0; i <= numeroFinal; i++)
             {
@@ -123,7 +165,7 @@ namespace ContadorNumeros
 
         static void ContarCincoEmCinco()
         {
-            int numeroFinal = PedirNumero();
+            int numeroFinal = PedirNumeroContar();
 
             for (int i = 0; i <= numeroFinal; i += 5)
             {
@@ -134,9 +176,42 @@ namespace ContadorNumeros
 
         static void ContarDezEmDez()
         {
-            int numeroFinal = PedirNumero();
+            int numeroFinal = PedirNumeroContar();
 
             for (int i = 0; i <= numeroFinal; i += 10)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void DecrementarUmEmUm()
+        {
+            int numeroDecrementar = PedirNumeroDecrementar();
+
+            for (int i = numeroDecrementar; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void DecrementarCincoEmCinco()
+        {
+            int numeroDecrementar = PedirNumeroDecrementar();
+
+            for (int i = numeroDecrementar; i >= 0; i -= 5)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void DecrementarDezEmDez()
+        {
+            int numeroDecrementar = PedirNumeroDecrementar();
+
+            for (int i = numeroDecrementar; i >= 0; i -= 10)
             {
                 Console.WriteLine(i);
                 Thread.Sleep(1000);
--- obj/Debug/net8.0/ContadorNumeros.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContadorNumeros.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContadorNumeros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2afc55b42d69e5b4bb466669aebf569952ec955")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5e19dae8f1a46236184af2f0a3486880bb41d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContadorNumeros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContadorNumeros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
